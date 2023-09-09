18f66e399a4d4b2a17a37b715313d1f4a2bbf156
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2023-09-09 05:54:38 -0600

    reworking the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,23 +5,30 @@ import Header from './Header';
 function Home() {
   return (
     <div className="landing-page">
-      <img src="https://evansmith93.github.io/mood-memo-site/assets/background.png" alt="Background image" className="bg-image" />
-      {/* Add padding and margin to raise the content up */}
-      <Header color="white" />
-      <Container className="content">
-        <h1 className="title mb-3">Mood Memo</h1>
-        <p className="description">A free app for tracking your mood and journaling daily</p>
-
-        <Row>
-          <Col>
-            <Button className="white-button mb-2" href='https://evansmith93.github.io/mood-memo-site/#/coming-soon'>Download for iOS</Button>
-          </Col>
-          <Col>
-            <Button className="white-button mb-2" href='https://play.google.com/store/apps/details?id=com.evansmith.mood_memo&hl=en_US&gl=US'>Download for Android</Button>
-          </Col>
-        </Row>
-
-      </Container>
+      <Row>
+        
+        <Col>
+        <div className='landing-page-left'>
+          <Header color="white" />
+          <Container className="content">
+            <h1 className="title mb-3">Mood Memo</h1>
+            <p className="description">A free app for tracking your mood and journaling daily</p>
+            <Row>
+              <Col>
+                <Button className="white-button mb-2" href='https://apps.apple.com/us/app/mood-memo/id6451342285'>Download for iOS</Button>
+              </Col>
+              <Col>
+                <Button className="white-button mb-2" href='https://play.google.com/store/apps/details?id=com.evansmith.mood_memo&hl=en_US&gl=US'>Download for Android</Button>
+              </Col>
+            </Row>
+          </Container>
+          </div>
+        </Col>
+        
+        <Col>
+          <img src="https://evansmith93.github.io/mood-memo-site/assets/no_background_screenshot.png"></img>
+        </Col>
+      </Row>
     </div>
   );
 }
